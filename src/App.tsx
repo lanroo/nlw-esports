38eb09997b3ad02d22cb2dcaba56da64e55e01cb
ylanna <yladacs@gmail.com>
2022-09-20 18:04:12 -0300

    bottom hover style

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20"> 
     <img src={logoImg} alt="" />
     <h1 className="text-6xl text-white font-black mt-20"> 
-      Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui.</h1>
+      Your <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> is here.</h1>
 
     <div className="grid grid-cols-6 gap-6 mt-16 ">
       <a href="" className="relative rounded-lg overflow-hidden ">
@@ -16,7 +16,7 @@ function App() {
 
         <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
          <strong className="font-bold text-white block">League of Legends</strong> 
-         <span className="text-zinc-300 text-sm block mt-1 "> 4 anúncios</span>
+         <span className="text-zinc-300 text-sm block mt-1 "> 4 ads</span>
         </div>
       </a>
 
@@ -25,7 +25,7 @@ function App() {
         
         <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
          <strong className="font-bold text-white block">Apex Legends</strong> 
-         <span className="text-zinc-300 text-sm block mt-1"> 4 anúncios</span>
+         <span className="text-zinc-300 text-sm block mt-1"> 4 ads</span>
         </div>
       </a>
 
@@ -34,7 +34,7 @@ function App() {
 
         <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
          <strong className="font-bold text-white block">Counter Strike</strong> 
-         <span className="text-zinc-300 text-sm block mt-1"> 4 anúncios</span>
+         <span className="text-zinc-300 text-sm block mt-1"> 4 ads</span>
         </div>
       </a>
 
@@ -43,7 +43,7 @@ function App() {
 
         <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
          <strong className="font-bold text-white block">World of Warcraft</strong> 
-         <span className="text-zinc-300 text-sm block mt-1"> 4 anúncios</span>
+         <span className="text-zinc-300 text-sm block mt-1"> 4 ads</span>
         </div>
       </a>
 
@@ -52,7 +52,7 @@ function App() {
 
         <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
          <strong className="font-bold text-white block">Dota 2</strong> 
-         <span className="text-zinc-300 text-sm block mt-1"> 4 anúncios</span>
+         <span className="text-zinc-300 text-sm block mt-1"> 4 ads</span>
         </div>
       </a>
 
@@ -61,15 +61,26 @@ function App() {
         
         <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
          <strong className="font-bold text-white block">fortnite</strong> 
-         <span className="text-zinc-300 text-sm block mt-1"> 4 anúncios</span>
+         <span className="text-zinc-300 text-sm block mt-1"> 4 ads</span>
         </div>
       </a>
 
     </div>
+         // text message and Post bottom 
 
+        <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden">
+        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+        
+        <div> 
+          <strong className="text-2xl text-white font-black block">Didn't find your duo?</strong>
+          <span className="text-zinc-400">Post an ad to find new players!</span>
+        </div>
 
-
-
+      <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded ">
+        Post an ad
+      </button>
+    </div>
+    </div>
     </div>
     
   )
