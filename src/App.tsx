b754eb6d3303cbf935f1eabacdc1af6dbde078bc
ylanna <yladacs@gmail.com>
2022-09-20 18:10:48 -0300

    add phosphor bottom

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { MagnifyingGlassPlus } from 'phosphor-react';
 
 import './styles/main.css';
 
@@ -66,8 +67,7 @@ function App() {
       </a>
 
     </div>
-         // text message and Post bottom 
-
+       
         <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden">
         <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
         
@@ -76,7 +76,8 @@ function App() {
           <span className="text-zinc-400">Post an ad to find new players!</span>
         </div>
 
-      <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded ">
+      <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+        <MagnifyingGlassPlus size={24}/>
         Post an ad
       </button>
     </div>
